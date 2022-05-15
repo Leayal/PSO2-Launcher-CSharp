b152e5049da949db80c709f32ba36a76ee142ba3
Dramiel Leayal <Leayal@users.noreply.github.com>
2022-05-15 19:00:49 +0700

    Insanity commit: Blazor site.
    - Tried blazor for website. It's huge, though.

---
+++ LauncherBlazorSite/Client/StaticResources.cs
@@ -0,0 +1,9 @@
+﻿using Octokit;
+
+namespace LauncherBlazorSite.Client
+{
+    static class StaticResources
+    {
+        public static readonly GitHubClient GithubClient = new GitHubClient(new ProductHeaderValue("PSO2LeaLauncherBlazorSite"));
+    }
+}
