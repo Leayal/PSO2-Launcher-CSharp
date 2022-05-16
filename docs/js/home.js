c6365309a27f7f12cf70a7802401ac3e9c0f6b45
Dramiel Leayal <Leayal@users.noreply.github.com>
2022-05-16 10:17:00 +0700

    Switch to SPA. Final draft for website.

--- docs/js/home.js
+++ docs/js/home.js
@@ -19,32 +19,19 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
         ghMentionsLink: true,
         openLinksInNewWindow: true,
         emoji: true
-    });
+    }), octokit = new Octokit({
+        userAgent: "PSO2LeaLauncherWeb v1.0"
+    }), content = d.getElementById("content");
     markdown.setFlavor('github');
 
-    const octokit = new Octokit({
-        userAgent: "PSO2LeaLauncherWeb v1.0"
-    });
-    const content = d.getElementById("content");
-    if (content) {
-        const _search = new URLSearchParams(w.location.search), currentpage = (_search.get("page") || "").toLowerCase();
-        if (currentpage == "downloads") {
-            RenderDownloads(content);
-        } else if (currentpage == "changelog") {
-            RenderChangelog(content);
-        } else {
-            RenderHome(content);
-        }
-    }
-
     function AddLoadingElement(parentDom) {
+        "use strict";
         const loader = d.createElement("div");
         const animation = d.createElement("div");
         animation.classList.add("cssload-tetrominos");
         for (let i = 1; i <= 4; i++) {
             const loader_box = d.createElement("div");
-            loader_box.classList.add("cssload-tetromino");
-            loader_box.classList.add("cssload-box" + i.toString());
+            loader_box.classList.add("cssload-tetromino", "cssload-box" + i.toString());
             animation.appendChild(loader_box);
         }
         loader.classList.add("loader3d");
@@ -53,25 +40,35 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
     }
 
     function RemoveLoadingElement(parentDom) {
+        "use strict";
         const loader = parentDom.querySelector(".loader3d");
         if (loader) {
             parentDom.removeChild(loader);
         }
     }
+
+    function RenderNotFound(parentDom) {
+        "use strict";
+        const notFound_title = d.createElement("h2");
+        notFound_title.textContent = "The document you requested doesn't exist.";
+        parentDom.appendChild(notFound_title);
+    }
     
     function RenderHome(parentDom) {
+        "use strict";
         const home_body = d.createElement("p"), home_title = d.createElement("h2");
         home_title.textContent = "Hello there.";
         home_body.textContent = "There's nothing to describe the launcher yet. But you can visit Downloads and Changelog page.";
         parentDom.appendChild(home_title);
         parentDom.appendChild(home_body);
-    }     
+    }
 
     async function RenderDownloads(parentDom) {
+        "use strict";
         AddLoadingElement(parentDom);
-        const latestReleaseInfo = (await octokit.rest.repos.getLatestRelease(targetRepo)).data;
+        const latestReleaseInfo = (await octokit.rest.repos.getLatestRelease(targetRepo)).data,
+        release_body = d.createElement("p"), release_title = d.createElement("h1"), release_assets = d.createElement("div"), title_download = d.createElement("h1");
         RemoveLoadingElement(parentDom);
-        const release_body = d.createElement("p"), release_title = d.createElement("h1"), release_assets = d.createElement("div"), title_download = d.createElement("h1");
         
         release_title.textContent = latestReleaseInfo.name;
         release_body.innerHTML = markdown.makeHtml(latestReleaseInfo.body);
@@ -94,10 +91,10 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
     }
 
     async function RenderChangelog(parentDom) {
+        "use strict";
         AddLoadingElement(parentDom);
-        const commitlogs = (await octokit.rest.repos.listCommits(targetRepo)).data;
+        const commitlogs = (await octokit.rest.repos.listCommits(targetRepo)).data, commit_list = d.createElement("div"), page_description = d.createElement("h1");
         RemoveLoadingElement(parentDom);
-        const commit_list = d.createElement("div"), page_description = d.createElement("h1");
         
         for (const commitData of commitlogs) {
             const commit_item = d.createElement("div"), commit_header = d.createElement("div");
@@ -137,4 +134,92 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
         parentDom.appendChild(page_description);
         parentDom.appendChild(commit_list);
     }
+
+    function clearChildNodes(element) {
+        "use strict";
+        while(element.firstChild) element.removeChild(element.lastChild);
+    }
+
+    function isDestinationPageSame(page1, page2) {
+        "use strict";
+        if (typeof (page1) === "string" && typeof (page2) === "string") return (page1.toLowerCase() === page2.toLowerCase());
+        else return (page1 == page2);
+    }
+
+    function isDestinationPageSameAsCurrent(dstPage) {
+        "use strict";
+        const _search = new URLSearchParams(w.location.search), currentPage = _search.get("page");
+        return isDestinationPageSame(dstPage, currentPage);
+    }
+
+    function navigatePage(currentpage) {
+        "use strict";
+
+        clearChildNodes(content);
+        if (currentpage) {
+            const comparand = currentpage.toLowerCase();
+            if (comparand == "downloads") {
+                RenderDownloads(content);
+            } else if (comparand == "changelog") {
+                RenderChangelog(content);
+            } else {
+                RenderNotFound;
+            }    
+        } else {
+            RenderHome(content);
+        }
+    }
+
+    const helperfunc_toggleClass = (assert, dom, ...classes) => (assert ? dom.classList.add : dom.classList.remove).call(dom.classList, classes);
+
+    // Get our entry to start after all DOM are ready to be manipulated.
+    w.addEventListener("DOMContentLoaded", function () {
+        "use strict";
+        const navigationItems = d.querySelectorAll(".navigation-list a");
+        if (navigationItems && navigationItems.length !== 0) {
+            
+            const eventCallback = function (e) {
+                "use strict";
+
+                // We yeet every chance that the browser will do its default behaviors.
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+
+                // Get the qualified and normalized "destination url".
+                const newUrl = new URL(e.target.href, w.location.href);
+
+                // Search for query "?page" and use its value
+                const _search = newUrl.searchParams, dstPage = _search.get("page");
+
+                if (!isDestinationPageSameAsCurrent(dstPage)) {
+                    // Get the current history state object of this window instance.
+                    const currentState = w.history.state;
+                    // Patch/replace the URL of the state object found above to change URL without reloading page.
+                    w.history.replaceState(currentState, "eh", newUrl.href);
+                    
+                    navigatePage(dstPage);
+                    
+                    for (let i = 0; i < navigationItems.length; i++) {
+                        const link = navigationItems.item(i);
+                        helperfunc_toggleClass(isDestinationPageSame((new URL(link.href)).searchParams.get("page"), dstPage), link, "current-page");
+                    }
+                }
+            };
+
+            for (let i = 0; i < navigationItems.length; i++) {
+                const link = navigationItems.item(i);
+                link.addEventListener("click", eventCallback, false);
+                const params = (new URL(link.href)).searchParams;
+                if (isDestinationPageSameAsCurrent(params.get("page"))) {
+                    link.classList.add("current-page");
+                }
+            }
+        }
+
+        if (content) {
+            const _search = new URLSearchParams(w.location.search);
+            navigatePage(_search.get("page"));
+        }
+    }, true);
 })(window, window.document);
